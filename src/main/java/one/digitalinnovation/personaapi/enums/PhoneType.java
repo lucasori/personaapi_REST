b954d48582da8ab7a58644c57b05e916487ed657
lucasori <lucasorihashi11@gmail.com>
2021-07-01 21:13:03 -0300

    Enums Phone

---
+++ src/main/java/one/digitalinnovation/personaapi/enums/PhoneType.java
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personaapi.enums;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    HOME("Home"),
+    MOBILE("Mobile"),
+    COMMERCIAL("Comercial");
+
+    private final String description;
+}
