a5f22d07aac94d8c0f3ae46ebccc06aac782ee4a
lucasori <lucasorihashi11@gmail.com>
2021-07-02 00:42:06 -0300

    Controller person

--- src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
@@ -1,17 +1,30 @@
 package one.digitalinnovation.personaapi.controller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import one.digitalinnovation.personaapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personaapi.entity.Person;
+import one.digitalinnovation.personaapi.repository.PersonReporsitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook(){
-        return "API Test";
+    private PersonReporsitory personReporsitory;
+
+    @Autowired
+    public PersonController(PersonReporsitory personReporsitory){
+        this.personReporsitory = personReporsitory;
+    }
+
+    @PostMapping
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+        Person savePerson = personReporsitory.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savePerson.getId())
+                .build();
     }
 
 }
