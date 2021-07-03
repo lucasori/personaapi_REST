7ef9c90cd172ac5e53e3d1286796e9c1ba2b36ea
lucasori <lucasorihashi11@gmail.com>
2021-07-03 00:21:32 -0300

    PersonController modificado

--- src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personaapi/controller/PersonController.java
@@ -4,27 +4,27 @@ package one.digitalinnovation.personaapi.controller;
 import one.digitalinnovation.personaapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personaapi.entity.Person;
 import one.digitalinnovation.personaapi.repository.PersonReporsitory;
+import one.digitalinnovation.personaapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/people")
 public class PersonController {
 
-    private PersonReporsitory personReporsitory;
+    private final PersonService personService;
 
     @Autowired
-    public PersonController(PersonReporsitory personReporsitory){
-        this.personReporsitory = personReporsitory;
+    public PersonController(PersonService personService) {
+        this.personService = personService;
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody Person person){
-        Person savePerson = personReporsitory.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savePerson.getId())
-                .build();
+
+        return personService.createPerson(person);
     }
 
 }
